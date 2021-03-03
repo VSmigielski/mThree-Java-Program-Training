8801edadfd18dab3d1cfd466353f1b22ddb1a05f
Veronica Smigielski <veronicasmigielski@gmail.com>
2021-03-03 22:15:44 +0100

    Edited and Added to Maven First Project, Created a Spring MVC ToDo List Application that works with in memory and with database profiles

--- todoDB Database/todoDB.sql
+++ todoDB Database/todoDB.sql
@@ -13,4 +13,9 @@ finished BOOLEAN DEFAULT false);
 INSERT INTO todo(id, todo, note, finished)
 VALUES(1, "Wash Car", "", false),
 (2, "Laundry", "Jeans", false),
-(3, "Wash dishes", "", true);
+(3, "Wash dishes", "", true),
+(4, "Play with Kitties", "Ball Roll", true),
+(5, "Unclog drain", "", false),
+(6, "Refrigerator Wipe", "Rotten food", false),
+(7, "Clean litter", "", true);
+
