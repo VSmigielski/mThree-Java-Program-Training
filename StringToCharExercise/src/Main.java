d82fc5c7a05aeea40805c365507e24612f7f2d10
Veronica Smigielski <veronicasmigielski@gmail.com>
2021-03-06 01:47:34 +0100

    Created 2 methods, one to return a built String, another to print the string through a loop on screen, and a main program to test

--- StringToCharExercise/src/Main.java
+++ StringToCharExercise/src/Main.java
@@ -2,32 +2,65 @@ import java.lang.reflect.Array;
 
 public class Main {
     public static void main(String[] args) {
+        // Use first method to auto-print on screen
         stringTo2CharString("Hello");
 
+        // Use first method to auto-print on screen
         stringTo2CharString("Hello there");
+        // Use first method to auto-print on screen
         stringTo2CharString("There");
 
+        // Create a new variable to store the return value
         String result = stringTo2CharString2("Hello");
+        // Print return value on screen
         System.out.println(result);
+
+        // Update value in the variable
         result = stringTo2CharString2("Hello there");
+        // Print return value on screen
         System.out.println(result);
+
+        // Update value in the variable
         result = stringTo2CharString2("There");
+        // Print return value on screen
         System.out.println(result);
     }
 
     public static void stringTo2CharString(String string) {
+        // Swap the string into an array
         char[] stringArray = string.toCharArray();
+        // Until the array is fully cycled through, continue the loop
         for (int i = 0; i < stringArray.length; i++) {
+            // Print the char on screen
             System.out.print(stringArray[i]);
+            // Print the char on screen (again)
             System.out.print(stringArray[i]);
         }
+        // Start on a fresh line
+        System.out.println("\r");
     }
 
-    public static String stringTo2CharString2(String s) {
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-
+    public static String stringTo2CharString2(String string) {
+        // Check if the string is empty
+        if (string == null) {
+            // If empty, return null
+            return null;
         }
-        return newString;
+
+        // Create a new string builder
+        StringBuilder output = new StringBuilder();
+
+        // Convert the string to a char array
+        char[] stringArray = string.toCharArray();
+
+        // Cycle through the array as long as the length isn't exceeded
+        for (int i = 0; i < stringArray.length; i++) {
+            // Append each character to the stringbuilder
+            output.append(stringArray[i]);
+            // Append each character to the stringbuilder (again)
+            output.append(stringArray[i]);
+        }
+        // Return the completed string
+        return output.toString().trim();
     }
 }
